6524cf34e4de19507248f494f54b091a097f42c7
chandanch <chandan.cec107@gmail.com>
2017-08-15 13:33:12 +0530

    code cleanup back to cli version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
-import {UserServiceService} from './user-service.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [UserServiceService]
 })
 export class AppComponent {
   title = 'Retracer App';
   private username : string;
-  constructor(private userService : UserServiceService) {
-    this.changeUserName();
-  }
-
-  changeUserName() {
-    setTimeout(() => {
-      this.username = this.userService.username;  
-    }, 3000);
+  constructor() {
   }
 }
